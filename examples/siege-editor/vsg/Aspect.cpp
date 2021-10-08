c7598aa5a5f6b9053a6edfd549254dc6b4b19109
sbrkopac <sbrkopac@users.noreply.github.com>
2021-10-08 17:19:19 -0600

    removed bogus for loop

--- examples/siege-editor/vsg/Aspect.cpp
+++ examples/siege-editor/vsg/Aspect.cpp
@@ -28,27 +28,26 @@ namespace ehb
         log->debug("asp has {} sub meshes", d->subMeshes.size());
 
         for (const auto& mesh : d->subMeshes)
-            for (auto kk = 0; kk < 1; ++kk)
+        {
+            log->debug("asp subMesh has {} textures", mesh.textureCount);
+
+            uint32_t f = 0; // track which face the loader is loading across the sub mesh
+            for (uint32_t i = 0; i < mesh.textureCount; ++i)
             {
-                log->debug("asp subMesh has {} textures", mesh.textureCount);
+                // create the geometry which will be drawn
+                // vsg::ref_ptr<vsg::Geometry> geometry = new vsg::Geometry;
 
-                uint32_t f = 0; // track which face the loader is loading across the sub mesh
-                for (uint32_t i = 0; i < mesh.textureCount; ++i)
-                {
-                    // create the geometry which will be drawn
-                    // vsg::ref_ptr<vsg::Geometry> geometry = new vsg::Geometry;
+                // textures are stored directly against the mesh
+                // TODO: should we store the vsg::Image against AspectImpl?
+                const std::string& imageFilename = d->textureNames[i];
 
-                    // textures are stored directly against the mesh
-                    // TODO: should we store the vsg::Image against AspectImpl?
-                    const std::string& imageFilename = d->textureNames[i];
+                auto vertices = vsg::vec3Array::create(mesh.cornerCount);
+                auto normals = vsg::vec3Array::create(mesh.cornerCount);
+                auto texCoords = vsg::vec2Array::create(mesh.cornerCount);
+                auto colors = vsg::vec3Array::create(mesh.cornerCount); // this is actually 4 colors but im lazy rn
 
-                    auto vertices = vsg::vec3Array::create(mesh.cornerCount);
-                    auto normals = vsg::vec3Array::create(mesh.cornerCount);
-                    auto texCoords = vsg::vec2Array::create(mesh.cornerCount);
-                    auto colors = vsg::vec3Array::create(mesh.cornerCount); // this is actually 4 colors but im lazy rn
-
-                    // this has to match the incoming pipe which was defined by the siege nodes
-                    auto attributeArrays = vsg::DataList{vertices, colors, texCoords};
+                // this has to match the incoming pipe which was defined by the siege nodes
+                auto attributeArrays = vsg::DataList{vertices, colors, texCoords};
 
 #if 0
                     // this is replaced by the attributeArrays call in vsg
@@ -58,63 +57,63 @@ namespace ehb
                     geometry->setColorArray(colors, vsg::Array::BIND_PER_VERTEX);
 #endif
 
-                    for (uint32_t cornerCounter = 0; cornerCounter < mesh.cornerCount; ++cornerCounter)
-                    {
-                        const auto& c = mesh.corners[cornerCounter];
-                        const auto& w = mesh.wCorners[cornerCounter];
+                for (uint32_t cornerCounter = 0; cornerCounter < mesh.cornerCount; ++cornerCounter)
+                {
+                    const auto& c = mesh.corners[cornerCounter];
+                    const auto& w = mesh.wCorners[cornerCounter];
 
-                        (*vertices)[cornerCounter] = w.position;
-                        (*texCoords)[cornerCounter] = c.texCoord;
-                        (*normals)[cornerCounter] = c.normal;
-                        (*colors)[cornerCounter] = vsg::vec3(c.color[0], c.color[1], c.color[2]);
-                    }
-
-                    auto elements = vsg::uintArray::create(mesh.matInfo[i].faceSpan * 3);
-                    //vsg::ref_ptr<vsg::DrawElementsUInt> elements = new vsg::DrawElementsUInt(GL_TRIANGLES);
-                    //geometry->addPrimitiveSet(elements);
-
-                    ///for (uint32_t fpt = 0; fpt < mesh.matInfo[i].faceSpan; ++fpt)
-                    for (uint32_t fpt = 0; fpt < mesh.matInfo[i].faceSpan * 3; fpt += 3)
-                    {
-                        (*elements)[fpt] = mesh.faceInfo.cornerIndex.at(f / 3).index[0] + mesh.faceInfo.cornerStart[i];
-                        (*elements)[fpt + 1] = mesh.faceInfo.cornerIndex.at(f / 3).index[1] + mesh.faceInfo.cornerStart[i];
-                        (*elements)[fpt + 2] = mesh.faceInfo.cornerIndex.at(f / 3).index[2] + mesh.faceInfo.cornerStart[i];
-
-                        f += 3;
-                    }
-
-                    // make sure to calculate our bounding boxes
-                    //geometry->setInitialBound(geometry->computeBoundingBox());
-
-                    // by default we draw static geometry
-                    //pseudoRoot->addChild(geometry);
-
-                    // make sure the skeleton is ready to go if we need it
-                    //skeleton->addChild(rigGeometry);
-
-                    if (auto textureData = vsg::read_cast<vsg::Data>(imageFilename, d->options); textureData != nullptr)
-                    {
-                        auto texture = vsg::DescriptorImage::create(vsg::Sampler::create(), textureData, 0, 0, VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER);
-                        assert(texture != nullptr);
-
-                        //! NOTE: should we be accessing the first element of the vector?
-                        auto descriptorSet = vsg::DescriptorSet::create(d->pipelineLayout->setLayouts[0], vsg::Descriptors{texture});
-                        assert(descriptorSet != nullptr);
-
-                        auto bindDescriptorSets = vsg::BindDescriptorSets::create(VK_PIPELINE_BIND_POINT_GRAPHICS, const_cast<vsg::PipelineLayout*>(d->pipelineLayout), 0, vsg::DescriptorSets{descriptorSet});
-                        assert(bindDescriptorSets != nullptr);
-
-                        addChild(bindDescriptorSets);
-                    }
-
-                    auto vid = vsg::VertexIndexDraw::create();
-                    vid->arrays = attributeArrays;
-                    vid->indices = elements;
-                    vid->indexCount = static_cast<uint32_t>(elements->size());
-                    vid->instanceCount = 1;
-
-                    addChild(vid);
+                    (*vertices)[cornerCounter] = w.position;
+                    (*texCoords)[cornerCounter] = c.texCoord;
+                    (*normals)[cornerCounter] = c.normal;
+                    (*colors)[cornerCounter] = vsg::vec3(c.color[0], c.color[1], c.color[2]);
                 }
+
+                auto elements = vsg::uintArray::create(mesh.matInfo[i].faceSpan * 3);
+                //vsg::ref_ptr<vsg::DrawElementsUInt> elements = new vsg::DrawElementsUInt(GL_TRIANGLES);
+                //geometry->addPrimitiveSet(elements);
+
+                ///for (uint32_t fpt = 0; fpt < mesh.matInfo[i].faceSpan; ++fpt)
+                for (uint32_t fpt = 0; fpt < mesh.matInfo[i].faceSpan * 3; fpt += 3)
+                {
+                    (*elements)[fpt] = mesh.faceInfo.cornerIndex.at(f / 3).index[0] + mesh.faceInfo.cornerStart[i];
+                    (*elements)[fpt + 1] = mesh.faceInfo.cornerIndex.at(f / 3).index[1] + mesh.faceInfo.cornerStart[i];
+                    (*elements)[fpt + 2] = mesh.faceInfo.cornerIndex.at(f / 3).index[2] + mesh.faceInfo.cornerStart[i];
+
+                    f += 3;
+                }
+
+                // make sure to calculate our bounding boxes
+                //geometry->setInitialBound(geometry->computeBoundingBox());
+
+                // by default we draw static geometry
+                //pseudoRoot->addChild(geometry);
+
+                // make sure the skeleton is ready to go if we need it
+                //skeleton->addChild(rigGeometry);
+
+                if (auto textureData = vsg::read_cast<vsg::Data>(imageFilename, d->options); textureData != nullptr)
+                {
+                    auto texture = vsg::DescriptorImage::create(vsg::Sampler::create(), textureData, 0, 0, VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER);
+                    assert(texture != nullptr);
+
+                    //! NOTE: should we be accessing the first element of the vector?
+                    auto descriptorSet = vsg::DescriptorSet::create(d->pipelineLayout->setLayouts[0], vsg::Descriptors{texture});
+                    assert(descriptorSet != nullptr);
+
+                    auto bindDescriptorSets = vsg::BindDescriptorSets::create(VK_PIPELINE_BIND_POINT_GRAPHICS, const_cast<vsg::PipelineLayout*>(d->pipelineLayout), 0, vsg::DescriptorSets{descriptorSet});
+                    assert(bindDescriptorSets != nullptr);
+
+                    addChild(bindDescriptorSets);
+                }
+
+                auto vid = vsg::VertexIndexDraw::create();
+                vid->arrays = attributeArrays;
+                vid->indices = elements;
+                vid->indexCount = static_cast<uint32_t>(elements->size());
+                vid->instanceCount = 1;
+
+                addChild(vid);
             }
+        }
     }
 } // namespace ehb
